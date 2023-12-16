0f00a1ad39e5cf5a4e71afecdde851b8964b7287
Alan Sarco <aljuniorsarco@gmail.com>
2023-12-16 16:05:40 +0800

    modify db connection

--- server/db_connection/dbConnection.js
+++ server/db_connection/dbConnection.js
@@ -3,10 +3,10 @@ const mysql2 = require('mysql2/promise');
 
 // create the damn connection
 const dbConnection = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USERNAME,
+    host: process.env.DB_HOST || process.env.DB_HOSTNAME,
+    user: process.env.DB_USERNAME || process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_DATABASE || process.env.DB_NAME,
     port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
@@ -14,16 +14,17 @@ const dbConnection = mysql.createPool({
 });
 
 const dbConnection2 = mysql2.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USERNAME,
+    host: process.env.DB_HOST || process.env.DB_HOSTNAME,
+    user: process.env.DB_USERNAME || process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_DATABASE || process.env.DB_NAME,
     port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
+
 module.exports = {
     dbConnection,
     dbConnection2
-}
+};
